dd7c45e27c27f5714f762a632dccac7bf59b7b6e
pmpuryaev <puryaev.pavel@gmail.com>
2022-06-25 17:29:58 +0300

    optimize imports

--- stepic_project_2/stepic_project_2/settings.py
+++ stepic_project_2/stepic_project_2/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -28,6 +29,7 @@ DEBUG = True
 ALLOWED_HOSTS = ['localhost', '127.0.0.1', '185.182.111.60', 'pmpuryaev.space', 'www.pmpuryaev.space']
 
 
+# 12345fffffffffffff
 # Application definition
 
 INSTALLED_APPS = [
@@ -119,6 +121,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
