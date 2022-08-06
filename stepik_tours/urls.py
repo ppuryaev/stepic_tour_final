8c04b84be14e6fb7a0039f87209a9c5cec15ae7f
pmpuryaev <puryaev.pavel@gmail.com>
2022-08-06 16:53:38 +0300

    Bug fixes. Review https://stepik.org/review/sessions/1393005

--- stepik_tours/urls.py
+++ stepik_tours/urls.py
@@ -30,8 +30,8 @@ handler500 = custom_handler500
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', main_view, name='main'),
-    path('departure/<str:departure>/', departure_view, name='departure_view'),
-    path('tour/<int:tour_id>/', tour_view, name='tour_view'),
+    path('departure/<str:departure>/', departure_view, name='departure'),
+    path('tour/<int:tour_id>/', tour_view, name='tour'),
 ]
 
 urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- tour/views.py
+++ tour/views.py
@@ -1,6 +1,6 @@
 import random
 
-from django.http import HttpResponseNotFound, HttpResponseServerError
+from django.http import HttpResponseNotFound, HttpResponseServerError, Http404
 from django.shortcuts import render
 
 import tour.data as dt
@@ -8,60 +8,64 @@ import tour.data as dt
 
 # Create your views here.
 def main_view(request):
-    tours_sample = dict(random.sample(dt.tours.items(), 6))
+    count_tours_main = 6
+    tours_sample = dict(random.sample(dt.tours.items(), count_tours_main))
     context = dict()
     correct_data = {}
     for tour_id, data in tours_sample.items():
         data['card_url'] = data['picture'].replace('&w=800&q=60', '&w=300&h=200')
-        data['price_view'] = '{:,}'.format(data['price']).replace(',', ' ')
+        # data['price_view'] = '{:,}'.format(data['price']).replace(',', ' ')
         correct_data[tour_id] = data
-    context['h1'] = "Всякие туры"
-    context['p1'] = dt.subtitle
-    context['p2'] = dt.description
+    # context['h1'] = "Всякие туры"
+    context['page_subtitle'] = dt.subtitle
+    context['page_description'] = dt.description
     context['data'] = correct_data
+    context['departures_nav'] = dt.departures
     return render(request, "index.html", context=context)
 
 
 def departure_view(request, departure):
+    departure_slug = dt.departures.get(departure, False)
+    if departure_slug is False:
+        raise Http404('Не найдено направление! ')
+
     filter_tours = dict([x for x in dt.tours.items() if x[1]['departure'] == departure])
     context = dict()
     context['dep_name'] = dt.departures.get(departure).split(' ')[-1]
     context['cnt_tours'] = len(filter_tours)
-    context['min_price'] = '{:,}'.format(min([filter_tours[x]['price'] for x in filter_tours])).replace(',', ' ')
-    context['max_price'] = '{:,}'.format(max([filter_tours[x]['price'] for x in filter_tours])).replace(',', ' ')
+    # context['min_price'] = '{:,}'.format(min([filter_tours[x]['price'] for x in filter_tours])).replace(',', ' ')
+    # context['max_price'] = '{:,}'.format(max([filter_tours[x]['price'] for x in filter_tours])).replace(',', ' ')
+    context['min_price'] = min([filter_tours[x]['price'] for x in filter_tours])
+    context['max_price'] = max([filter_tours[x]['price'] for x in filter_tours])
     context['min_date'] = min([filter_tours[x]['nights'] for x in filter_tours])
     context['max_date'] = max([filter_tours[x]['nights'] for x in filter_tours])
     correct_data = {}
     for tour_id, data in filter_tours.items():
         data['card_url'] = data['picture'].replace('&w=800&q=60', '&w=300&h=200')
-        data['price_view'] = '{:,}'.format(data['price']).replace(',', ' ')
+        # data['price_view'] = '{:,}'.format(data['price']).replace(',', ' ')
         correct_data[tour_id] = data
     context['data'] = correct_data
+    context['departures_nav'] = dt.departures
     return render(request, "departure.html", context=context)
 
 
 def tour_view(request, tour_id):
     tour = dt.tours.get(tour_id, False)
     if tour is False:
-        return HttpResponseNotFound("Нет такого тура! Выбери другой.")
-    st = [x for x in range(int(tour['stars']))]
+        raise Http404('Не найден тур! ')
+    stars = [x for x in range(int(tour['stars']))]
     context = tour
-    context['rangeSt'] = st
-    context['price_view'] = '{:,}'.format(context['price']).replace(',', ' ')
-    dep_list = {
-        "nsk": "Из Новосибирска",
-        "ekb": "Из Екатеринбурга",
-        "msk": "Из Москвы",
-        "spb": "Из Петербурга",
-        "kazan": "Из Казани"
-    }
-    context['dep_name'] = dep_list.get(context.get('departure'))
+    context['rangeSt'] = stars
+    # context['price_view'] = '{:,}'.format(context['price']).replace(',', ' ')
+    dep_list = dt.departures
+    context['dep_name'] = dep_list.get(context.get('departure')).lower()
+    context['departures_nav'] = dt.departures
     return render(request, "tour.html", context=context)
 
 
 def custom_handler404(request, exception):
     # Call when Http404 raised
-    return HttpResponseNotFound('Ресурс не найден!')
+    return HttpResponseNotFound("{}Выбери из существующих вариантов.".format(exception))
 
 
 def custom_handler500(request):
